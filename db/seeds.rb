c7ea4b7e2ae84535761f77bed99db3933162e820
ricardo39985 <ricardo39985@gmail.com>
2020-02-26 15:10:25 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+include Faker
+50.times do
+  Post.create(
+    title: Faker::Books::Lovecraft.tome,
+    description: Faker::Books::Lovecraft.paragraph
+  )
+end
